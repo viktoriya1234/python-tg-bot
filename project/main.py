3ea9fc0caa72921f297f44fa1c5d36ab9e101e27
viktoriya1234 <vikapav007@gmail.com>
2025-02-01 14:16:42 +0200

    ls10

--- project/main.py
+++ project/main.py
@@ -3,6 +3,8 @@ import threading
 import config as c
 from SQliteManager import SQLNote
 import telebot
+import re
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 bot = telebot.TeleBot(c.BOT_TOKEN)
 
@@ -20,7 +22,7 @@ bot = telebot.TeleBot(c.BOT_TOKEN)
 #
 # cur.execute('''CREATE TABLE IF NOT EXISTS notes (
 #     id INTEGER PRIMARY KEY AUTOINCREMENT,
-#     chat_id INTEGER NOT NULL,
+#     user_id INTEGER NOT NULL,
 #     title TEXT NOT NULL,
 #     content TEXT DEFAULT '',
 #     notification DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -42,7 +44,7 @@ def send_text_message():
 
 
 def bot_start(message):
-    print(message)
+    # print(message)
     with get_bd_cursor() as cur:
         cur.execute("SELECT chat_id FROM users WHERE chat_id='%d'" % message.chat.id)
         row = cur.fetchone()
@@ -53,8 +55,54 @@ def bot_start(message):
         else:
             bot.send_message(message.chat.id, 'Ви вже підписалися на цього бота')
 
+def bot_add_note(message):
+    bot.send_message(message.chat.id, 'Введіть нотатку')
+    bot.register_next_step_handler(message, bot_add_title)
+
+
 def bot_add_title(message):
-    bot.send_message(message.chat.id, 'Нотатку додано!')
+    with get_bd_cursor() as cur:
+        cur.execute(f"SELECT id FROM users WHERE chat_id={int(message.chat.id)}")
+        row = cur.fetchone()
+
+        if row:
+            cur.execute("INSERT INTO notes (user_id, title) VALUES(?, ?)",
+                        (row[0], message.text))
+            bot.send_message(message.chat.id, 'Нотатку додано!')
+        else:
+            bot.send_message(message.chat.id, 'Нема користувача :')
+
+
+def all_notes(message):
+    with get_bd_cursor() as cur:
+        cur.execute(f"SELECT id FROM users WHERE chat_id={int(message.chat.id)}")
+        row = cur.fetchone()
+
+        if row:
+            cur.execute(f"SELECT id, title, notification FROM notes WHERE user_id={row[0]}")
+            rows = cur.fetchall()
+
+            notes = ''
+            for r in rows:
+                notes += f"/edit_{r[0]}: {r[1]}. [{r[2]}\n]"
+
+            if notes:
+                bot.send_message(message.chat.id, notes)
+            else:
+                bot.send_message(message.chat.id, 'Нотаток немає!')
+
+
+def edit_note(message, note_id:int = 0):
+    keyboard = InlineKeyboardMarkup()
+    b1 = InlineKeyboardButton('Заголовок', callback_data='title_')
+    b2 = InlineKeyboardButton('Опис', callback_data='content_')
+    b3 = InlineKeyboardButton('Час', callback_data='notification_')
+    b4 = InlineKeyboardButton('Видалити', callback_data='delete_')
+    keyboard.add(b1, b2, b3)
+    keyboard.add(b4)
+
+    bot.send_message(message.chat.id, 'Виберіть дію.\nРедагувати:',
+                     reply_markup=keyboard)
 
 # === MESSAGE-HANDLERS ===============================
 
@@ -66,15 +114,24 @@ def bot_add_title(message):
 # help - вивести підказки
 # end - відписатися
 
-@bot.message_handler(commands=['start', 'add', 'edit', 'del', 'help', 'end'])
+@bot.message_handler(commands=['start', 'add', 'edit', 'del', 'help', 'end', 'all'])
 def bot_commands(message):
     if '/start' == message.text:
-        bot_start()
+        bot_start(message)
     elif '/add' == message.text:
-        bot.send_message(message.chat.id, 'Введіть нотатку')
-        bot.register_next_step_handler(message, bot_add_title)
+        bot_add_note(message)
     elif '/edit' == message.text:
         pass
+    elif '/all' == message.text:
+        all_notes(message)
+
+
+@bot.message_handler(regexp=r"^\/edit_\d+$",)
+def handler_edit_id(message):
+    match = re.match(r"^\/edit_(\d+)$", message.text)
+    if match:
+        edit_note(message,int(match.group(1)))
+
 
 @bot.message_handler(content_types=['text'])
 def text_massage(message):
